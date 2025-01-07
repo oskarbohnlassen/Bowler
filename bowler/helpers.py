226533d3199e7810f8691e80b8ce7e5869fabb78
Oskar Bohn Lassen <obola@dtu.dk>
2025-01-07 10:49:20 +0100

    Update helpers.py

--- bowler/helpers.py
+++ bowler/helpers.py
@@ -4,6 +4,7 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
+import numpy
 
 import logging
 from typing import List, Optional, Sequence, Union
